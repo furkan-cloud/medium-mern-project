5300e713bbc11fe3e9de24c837f63834b77921da
Gizem <whiteswan1697@gmail.com>
2020-12-16 18:30:43 +0300

    CardList and cardData created

---
+++ src/components/cardList/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from '../card/Card';
+import {cardContent} from '../../data/cardData';
+
+
+  const CardList = () => {
+      console.log(cardContent)
+     return(
+        <div> 
+            {cardContent.map((content,index)=>
+                <Card
+                    userprofile={content.userprofile}
+                    username={content.username}
+                    imageUrl={content.imageUrl}
+                    title={content.title}
+                    date={content.date}
+                    key={index}
+                />
+             
+            )}  
+        </div>
+        
+     )
+  } 
+
+export default CardList;
