427d6384f22d7c2dcb535841bcc090d0b825a312
Gizem <whiteswan1697@gmail.com>
2020-12-21 10:53:53 +0300

    Added ArticleDetailCard component, ProfileDetailList page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar.js';
-import FollowList from './components/followList/FollowList';
-import TopicsList from './components/topicsToFollow/TopicsList';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import MainCard from './components/mainCard/MainCard';
-import SectionOne from './components/sectionOne/SectionOne';
-import SectionTwo from './components/sectionTwo/SectionTwo';
-import SectionThree from './components/sectionThree/SectionThree';
 import Home from './pages/homePage/Home';
 import ArticleDetails from './pages/articleDetail/ArticleDetails';
+import ProfileDetailList from './pages/profileDetailList/ProfileDetailList';
 
 
 function App() {
@@ -17,8 +12,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
+          <Route path='/profileDetail' component={ProfileDetailList} exact />
           <Route path='/articleDetails' component={ArticleDetails} exact />
           <Route path='/' component={Home} />
+          
         </Switch>
       </div>
     </BrowserRouter>
@@ -26,14 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-{/* <BrowserRouter>
-<NewsContext.Provider value={{newsList, setNewsList, categorySource, selectedCategory, setSelectedCategory, selectedNews, setSelectedNews }}>
-  <NavBar />
-  <Switch>
-    <Route path='/details' component={NewsDetails} exact />
-    <Route path='/' component={NewsBanner} />
-  </Switch>
-</NewsContext.Provider>
-</BrowserRouter> */}
