25d7d42423230791935970b8d36b4d87c3f8d771
Gizem <whiteswan1697@gmail.com>
2020-12-22 09:14:46 +0300

    Fixed of components style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, {useState, createContext,useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/navbar/Navbar.js';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/homePage/Home';
 import ArticleDetails from './pages/articleDetail/ArticleDetails';
 import ProfileDetailList from './pages/profileDetailList/ProfileDetailList';
+
+
+// const fetchArticles = async ()
+
+// const articleSearch = (article) => {
+//   const filteredList = originalList.filter((item) => {
+//     const userText = text.toUpperCase();
+//     const cityName = item.toUpperCase();
+//     return cityName.indexOf(userText) > -1;
+//   });
+//   setCityList(filteredList);
+// };
 
 
 function App() {
--- src/components/bigCardList/BigCardList.js
+++ src/components/bigCardList/BigCardList.js
@@ -15,8 +15,7 @@ const BigCardList = () => {
                   description={content.description}
                   date={content.date}
                   key={index}
-              />
-           
+              />           
           )}  
       </div>
       
