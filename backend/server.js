5642d8b911ee448cd04d7c69fd189f9288a7c073
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-07 11:17:42 +0300

    server updated

--- backend/server.js
+++ backend/server.js
@@ -27,13 +27,14 @@ app.use("/api", router);
 //Error Handler
 app.use(customErrorHandler);
 //Static Files
-app.use(express.static('../build'));
+// app.use(express.static('../build'));
 
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    // res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build/index.html"));
   });
 }
 
