78e40369e4824837bca06dbf02809c0a77a41948
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-08 13:24:24 +0300

    reading list link added

--- client/src/components/readingCard/ReadingCard.js
+++ client/src/components/readingCard/ReadingCard.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ReadingCard.css";
+import { useHistory } from "react-router-dom";
 
 const ReadingCard = (props) => {
+  const history = useHistory();
+  const handleOnClickArticle = () => {
+    history.push(`/articleDetail/${props.id}`)
+  };
+  const handleOnClick = () => {
+      history.push(`/profileDetail/${props.authorId}`)
+  };
   return (
     <div className="readingcard-container">
       <div className="readingtext-container">
-        <div className="readingusername">
+        <div onClick={handleOnClick} className="readingusername">
           <img
             className="readingProfileImage"
             src={props.profileImage}
@@ -13,7 +21,7 @@ const ReadingCard = (props) => {
           />
           <div>{props.firstName + " " + props.lastName}</div>
         </div>
-        <div className="readingtitle">{props.title}</div>
+        <div onClick={handleOnClickArticle} className="readingtitle">{props.title}</div>
         <div className="readingdate">{props.date}</div>
       </div>
     </div>
