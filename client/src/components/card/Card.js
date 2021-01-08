be6ada89933bbf4e50892226c0b0e121a91f484d
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-08 09:54:11 +0000

    deleted unused files, added some styles, added delete button to my profile page

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Card.css";
+import axios from 'axios'
+import { useContext } from "react";
+import UserContext from "../../context/UserContext";
 
 const Card = (props) => {
+  const { userData, setUserData } = useContext(UserContext)
+
+  const handleDelete = async () => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      const articleDeleteResponse = await axios.delete(
+        `http://localhost:5000/api/posts/${props.id}/delete`,
+        {
+          headers: {
+            "x-auth-token": token,
+          },
+        }
+      );
+      setUserData({ ...userData, user: articleDeleteResponse.data })
+      console.log(articleDeleteResponse.data)
+    }
+  };
   return (
     <div className="card-container">
       <div className="text-container">
@@ -13,7 +33,7 @@ const Card = (props) => {
               src={props.profileImage}
               alt=""
             />
-            {props.username}
+            <span>{props.username}</span>
           </div>
         </Link>
         <Link to={`/articleDetail/${props.id}`}>
@@ -24,7 +44,11 @@ const Card = (props) => {
 
       <div className="image-container">
         <img className="card-image" src={props.imageUrl} alt="" />
+        {
+          props.delete && <div className = 'articleDeleteButton' onClick={handleDelete}>Delete</div>
+        }
       </div>
+
     </div>
   );
 };
